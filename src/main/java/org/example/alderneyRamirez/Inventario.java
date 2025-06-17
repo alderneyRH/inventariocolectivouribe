90812874b0e3c5cb64addfa8bf89ad976a8af13a
alderney ramirez <alderneyramirez@gmail.com>
2025-06-17 15:13:29 -0500

    se crea las variables principales y el menu de opciones del proyecto

---
+++ src/main/java/org/example/alderneyRamirez/Inventario.java
@@ -0,0 +1,33 @@
+package org.example.alderneyRamirez;
+
+import java.util.Scanner;
+
+public class Inventario {
+
+        public static void main(String[] args) {
+
+            //ENTRADAS
+            Scanner keyEntry = new Scanner(System.in);
+            Integer menuOption = null;
+            String colorVerde = "\u001B[32m";
+            //proeceso 1 (CREAR EL MENU DE OPCIONES)
+            System.out.println("\n***********************************");
+            System.out.println("💼 BIENVENIDO A URIBE APP - GESTOR DE PRENDAS");
+            System.out.println("***********************************\n");
+
+            System.out.println(colorVerde+"📋 ¿Qué acción deseas realizar?");
+            System.out.println("1️⃣  Crear una prenda en BD 🧥");
+            System.out.println("2️⃣  Mostrar el inventario de prendas 📦");
+            System.out.println("3️⃣  Modificar los datos de una prenda ✏️");
+            System.out.println("4️⃣  Eliminar una prenda 🗑️");
+            System.out.println("5️⃣  Salir de la aplicación 🚪");
+
+            while(menuOption!=5){
+              System.out.println("oe");
+            }
+
+
+            //proceso 2
+        }
+    }
+
